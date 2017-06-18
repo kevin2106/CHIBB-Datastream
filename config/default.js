b63be926ed0e89bfc09630b05b78c88f7de200e1
KevinPC <tehennepek@gmail.com>
2017-06-18 23:23:24 +0200

    CHANGED: Changed the way data is put into the database.

---
+++ config/default.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongo: {
+    url: 'mongodb://127.0.0.1:27017/local',
+    collection: 'Directiekade 2-8, 3089 JA Rotterdam'
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var mqtt = require('mqtt');
+var timestamp = require('unix-timestamp');
 var _ = require('lodash');
 const config = require('config');
 const mongoDBClient = require('mongodb').MongoClient;
+
 
 let db;
 
@@ -19,29 +21,33 @@ mongoDBClient.connect(config.mongo.url)
 const clientOptions = {
     port: 8083,
     clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-    username: config.mqtt.username,
-    password: config.mqtt.password
+    username: 'student_api',
+    password: 'QL#vo9v1h6w6whX'
 };
 
-var client = mqtt.connect(config.mqtt.url, clientOptions);
+var client = mqtt.connect('wss://mqtt.chibb-box.nl', clientOptions);
 
 client.on('connect', function () {
   client.subscribe('sensors')
-  console.log("Subscribed to sensors channel")
+  console.log("Subscribed to sensors channel");
 })
 
-  client.on('message', function (topic, message) {
+client.on('message', function (topic, message) {
     const object = JSON.parse(message);
     _.forEach(object.readings, function(value, key) {
-      var values = {"Timestamp": value.timestamp,
+      console.log(timestamp.toDate(value.timestamp));
+      var values = {"TimeStamp": timestamp.toDate(value.timestamp),
                      "Value":   value.reading};
-      db.update({ "Type" : value.type }, { $push: { "Readings" : values }})
-        .catch(err => {
-          console.error(err);
-        });
-      console.log("Value: " + value.type + " : " + "Key: " + key );
+      if(value.type == 'temperature'){
+        db.update({ "Name" : "Woonkamer" }, { $push: { "Readings" : values }})
+          .catch(err => {
+            console.error(err);
+          });
+        console.log("Value: " + value.type + " : " + "Key: " + key );
+      } 
     });
   });
+
 
 
 /* GET home page. */
